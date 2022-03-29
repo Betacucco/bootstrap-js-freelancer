9666d26bb69c9e8425c1cb1428a5aa888c1d503b
MarcoPieretti <marcopieretti50@gmail.com>
2022-03-29 20:19:57 +0200

    Aggiunta DiscountCode

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-let arrayOfDiscountCode = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
-
-//let discountCode = document.getElementById("discountCode").value;
 
 
 document.getElementById("sendDate").addEventListener("click",calcolaPrezzo);
@@ -8,17 +5,36 @@ document.getElementById("sendDate").addEventListener("click",calcolaPrezzo);
  function calcolaPrezzo(event){
     event.preventDefault();
 
+    //let flag = false; 
     let typeOfWork = document.getElementById("typeOfWork").value;
-    let hoursRequested = parseInt(document.getElementById("hoursRequested").value);
+    let hoursRequested = parseInt(document.getElementById("hoursRequested").value); 
+    let controlloSconto = controlloDiscountCode();
 
-    if(typeOfWork == 1){
+    if(typeOfWork == 1 && !controlloSconto){
         price = (20.5 * hoursRequested);
-    }else if(typeOfWork == 2){
+    } else if(typeOfWork == 1 && controlloSconto){
+        price = (20.5 * hoursRequested) - ((20.5 * hoursRequested) * 0.25);
+    } else if(typeOfWork == 2 && !controlloSconto){
         price = (15.3 * hoursRequested);
-    } else if(typeOfWork == 3){
+    } else if(typeOfWork == 2 && controlloSconto){
+        price = (15.3 * hoursRequested) - ((20.5 * hoursRequested) * 0.25);
+    } else if(typeOfWork == 3 && !controlloSconto){
         price = (33.6 * hoursRequested);
+    } else if(typeOfWork == 3 && controlloSconto){
+        price = (33.6 * hoursRequested) - ((20.5 * hoursRequested) * 0.25);
     }
-    document.getElementById("prezzoTotale").innerHTML = "Il prezzo totale e' di: " + price;
+    document.getElementById("prezzoTotale").innerHTML = "Il prezzo totale e' di: " + price + "\u20AC";
+  } 
+
+function controlloDiscountCode(){
+    let arrayOfDiscountCode = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
+    let discountCode = document.getElementById("discountCode").value;
+    let flag = false;
+    
+    for(let i = 0; i < arrayOfDiscountCode.length; i++){
+        if(discountCode == arrayOfDiscountCode[i]){
+             flag = true;
+        }
+    }
+    return flag;
 }
-
-
