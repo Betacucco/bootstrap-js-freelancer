c5376fb4f6feffed91c3fe2bba8283ab0a7e0003
MarcoPieretti <marcopieretti50@gmail.com>
2022-03-29 22:53:00 +0200

    Responsive tranne per la navbar

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-
-
+//Dichiaro l'array di codici sconto
+let arrayOfDiscountCode = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
+//Aggiungiamo l'evento click 
 document.getElementById("sendDate").addEventListener("click",calcolaPrezzo);
-
+//Funzione che calcola il prezzo in base al tipo di lavoro e al controllo sconto 
  function calcolaPrezzo(event){
     event.preventDefault();
 
-    //let flag = false; 
     let typeOfWork = document.getElementById("typeOfWork").value;
     let hoursRequested = parseInt(document.getElementById("hoursRequested").value); 
     let controlloSconto = controlloDiscountCode();
@@ -23,18 +23,27 @@ document.getElementById("sendDate").addEventListener("click",calcolaPrezzo);
     } else if(typeOfWork == 3 && controlloSconto){
         price = (33.6 * hoursRequested) - ((20.5 * hoursRequested) * 0.25);
     }
-    document.getElementById("prezzoTotale").innerHTML = "Il prezzo totale e' di: " + price + "\u20AC";
-  } 
+    
+    document.getElementById("prezzoTotale").innerHTML = "Il prezzo totale e' di: " + price.toFixed(2) + "\u20AC";
+}
 
+/*Funzione che controlla se il codice inserito e' valido
+se lo e' lo elimina dall'array*/
 function controlloDiscountCode(){
-    let arrayOfDiscountCode = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
     let discountCode = document.getElementById("discountCode").value;
     let flag = false;
     
     for(let i = 0; i < arrayOfDiscountCode.length; i++){
         if(discountCode == arrayOfDiscountCode[i]){
              flag = true;
+             arrayOfDiscountCode.splice(i, 1);
+             //console.log(arrayOfDiscountCode);
         }
     }
+    //Se non e' valido o vuoto lancia un alert di errore 
+    if(!flag || discountCode == ""){
+        alert("Il codice e' sbagliato o non e' stato inserito");
+    }
+
     return flag;
 }
